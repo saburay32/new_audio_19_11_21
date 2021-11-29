4fab3a776e9a505192ad37064e9253cab448a9c9
Saburay32 <saburay32@gmail.com>
2021-11-29 19:17:50 +0300

    пробую писать обработку события

--- graphic_interface/gui.py
+++ graphic_interface/gui.py
@@ -1,17 +1,45 @@
-import PySimpleGUI as sg
+from PyQt5 import uic, QtWidgets
 
-sg.theme('Reddit')
+Form, _ = uic.loadUiType("First.ui")
 
-layout = [[sg.Text('My next gui panel')],
-          [sg.Text('Enter ===>'),sg.InputText()],
-          [sg.Button('OK'),sg.Button('Cancel')]]
+class Ui(QtWidgets.QDialog,Form):
+    def __init__(self):
+        super(Ui, self).__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.printButtonPressed)
 
-window = sg.Window('GuiPanel',layout)
+    def printButtonPressed(selfs):
+        print("pressed")
 
-while True:
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED or event == 'Cancel':
-        break
-    print('You entered ',values[0])
+if __name__ == '__main__':
+    import sys
 
-window.close()
+    app = QtWidgets.QApplication(sys.argv)
+    w = Ui()
+    w.show()
+    sys.exit(app.exec_())
+# app = QApplication([])
+# window = Window()
+# form = Form()
+# form.setupUi(window)
+# window.show()
+# app.exec()
+
+
+# import PySimpleGUI as sg
+#
+# sg.theme('Reddit')
+#
+# layout = [[sg.Text('My next gui panel')],
+#           [sg.Text('Enter ===>'),sg.InputText()],
+#           [sg.Button('OK'),sg.Button('Cancel')]]
+#
+# window = sg.Window('GuiPanel',layout)
+#
+# while True:
+#     event, values = window.read()
+#     if event == sg.WINDOW_CLOSED or event == 'Cancel':
+#         break
+#     print('You entered ',values[0])
+#
+# window.close()
